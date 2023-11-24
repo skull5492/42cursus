dbf15d031196571f150841ff5fa60d62c1bfc25d
skull5492 <79159643+skull5492@users.noreply.github.com>
2023-11-24 14:59:48 +0000

    bruh

---
+++ push_swap/includes/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtorreir <jtorreir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 17:06:04 by jtorreir          #+#    #+#             */
+/*   Updated: 2023/10/26 17:06:04 by jtorreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <string.>
+# include <stdlib.h>
+# include <stdio.h>
+# include <errno.h>
+
+#endif
---
+++ push_swap/srcs/operations/rotate.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtorreir <jtorreir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 17:31:59 by jtorreir          #+#    #+#             */
+/*   Updated: 2023/10/26 17:31:59 by jtorreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+void	rotate_stack_a(long *stack_a, int *numbers_in_stack_a)
+{
+	int		i;
+	long	*temp;
+
+	i = 0;
+	temp = (long *)malloc(sizeof(long));
+	*temp = stack_a[0];
+	while (i < numbers_in_stack_a[1])
+	{
+		stack_a[i] = stack_a[i + 1];
+		i++;
+	}
+	stack_a[numbers_in_stack_a[1] - 1] = *temp;
+	free(temp);
+	write(1, "Rotate Stack A\n", 15);
+}
+
+void	rotate_stack_b(long *stack_b, int *numbers_in_stack_b)
+{
+	int		i;
+	long	*temp;
+
+	i = 0;
+	temp = (long *)malloc(sizeof(long));
+	*temp = stack_b[0];
+	while (i < *numbers_in_stack_b)
+	{
+		stack_b[i] = stack_b[i + 1];
+		i++;
+	}
+	stack_b[*numbers_in_stack_b - 1] = *temp;
+	free(temp);
+	write(1, "Rotate Stack B\n", 15);
+}
+
+void	rotate_stack_a_and_stack_b(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b)
+{
+	int		i;
+	long	*temp;
+
+	i = 0;
+	temp = (long *)malloc(sizeof(long));
+	*temp = stack_a[0];
+	while (i < numbers_in_stack_a[1])
+	{
+		stack_a[i] = stack_a[i + 1];
+		i++;
+	}
+	stack_a[numbers_in_stack_a[1] - 1] = *temp;
+	i = 0;
+	*temp = stack_b[0];
+	while (i < *numbers_in_stack_b)
+	{
+		stack_b[i] = stack_b[i + 1];
+		i++;
+	}
+	stack_b[*numbers_in_stack_b - 1] = *temp;
+	free(temp);
+	write(1, "Rotate both Stack A and Stack B\n", 44);
+}
