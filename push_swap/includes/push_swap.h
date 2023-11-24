9d9a45e22ff4d6a7e5639010a53e75caea51e755
skull5492 <79159643+skull5492@users.noreply.github.com>
2023-11-24 15:14:20 +0000

    pushing

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -19,4 +19,20 @@
 # include <stdio.h>
 # include <errno.h>
 
+void    stack_a_push(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers);
+void    stack_b_push(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers);
+
+void    reverse_rotate_stack_a(long *stack_a, int *stack_a_numbers);
+void    reverse_rotate_stack_b(long *stack_b, int *stack_b_numbers);
+void    reverse_both_stacks(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers);
+
+void	rotate_stack_a(long *stack_a, int *numbers_in_stack_a);
+void	rotate_stack_b(long *stack_b, int *numbers_in_stack_b);
+void	rotate_stack_a_and_stack_b(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
+
+void    swap_stack_a(long *stack_a, int *stack_a_numbers);
+void    swap_stack_b(long, *stack_b, int *stack_b_numbers);
+void    swap_both_stacks(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers);
+
+void    sort_3_numbers(long *stack_a, int *stack_a_numbers);
 #endif
--- push_swap/srcs/operations/swap.c
+++ push_swap/srcs/operations/swap.c
@@ -42,7 +42,7 @@ void   swap_stack_b(long, *stack_b, int *stack_b_numbers)
     write(1, "Swap Stack B\n", 13);
 }
 
-void    swap_both_stacks(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers)
+void   swap_both_stacks(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers)
 {
     long *temp3
 
