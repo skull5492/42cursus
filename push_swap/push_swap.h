e3145bc236c45380a61067e99d5eb0a2f98bf02a
skull5492 <79159643+skull5492@users.noreply.github.com>
2023-12-21 13:39:12 +0000

    bruh

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -19,22 +19,25 @@
 # include <stdio.h>
 # include <errno.h>
 
-void    stack_a_push(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers);
-void    stack_b_push(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers);
+void	stack_a_push(long *stack_a, long *stack_b, 
+\int *stack_a_numbers, int *stack_b_numbers);
+void	stack_b_push(long *stack_a, long *stack_b,
+\int *stack_a_numbers, int *stack_b_numbers);
 
-void    reverse_rotate_stack_a(long *stack_a, int *stack_a_numbers);
-void    reverse_rotate_stack_b(long *stack_b, int *stack_b_numbers);
-void    reverse_both_stacks(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers);
+void	reverse_rotate_stack_a(long *stack_a, int *stack_a_numbers);
+void	reverse_rotate_stack_b(long *stack_b, int *stack_b_numbers);
+void	reverse_both_stacks(long *stack_a, long *stack_b, 
+\int *stack_a_numbers, int *stack_b_numbers);
 
 void	rotate_stack_a(long *stack_a, int *stack_a_numbers);
 void	rotate_stack_b(long *stack_b, int *stack_b_numbers);
 void	rotate_stack_a_and_stack_b(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers);
 
-void    swap_stack_a(long *stack_a, int *stack_a_numbers);
-void    swap_stack_b(long, *stack_b, int *stack_b_numbers);
-void    swap_both_stacks(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers);
+void	swap_stack_a(long *stack_a, int *stack_a_numbers);
+void	swap_stack_b(long, *stack_b, int *stack_b_numbers);
+void	swap_both_stacks(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers);
 
-void    sort_3_numbers(long *stack_a, int *stack_a_numbers);
+void	sort_3_numbers(long *stack_a, int *stack_a_numbers);
 void	sort_5_numbers(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers);
 void	sort_beyond_3_and_5_numbers(long *stack_a, long *stack_b, int *stack_a_numbers, int *stack_b_numbers);
 
